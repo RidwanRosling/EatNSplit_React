904aa0854cb08e7f4f33bdbe3e91a4fc39add1b1
RidwanRosling <ridwansbdt@gmail.com>
2025-04-05 14:03:48 +0700

    adding handle Add friend to make new friend

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,36 @@ const initialFriends = [
 
 export default function App() {
   const [showFriends, setShowFriends] = useState(false);
+  const [friendName, setFriendName] = useState("");
+  const [newImg, setNewImg] = useState("https://i.pravatar.cc/48?u=49");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!friendName) {
+      alert("Please enter a name");
+      return;
+    }
+    if (!newImg) {
+      alert("Please enter an image URL");
+      return;
+    }
+    if (!newImg.startsWith("https://")) {
+      alert("Please enter a valid image URL");
+      return;
+    }
+
+    const newFriend = {
+      id: crypto.randomUUID(),
+      name: friendName,
+      image: `${newImg}`,
+      balance: 0,
+    };
+
+    initialFriends.push(newFriend);
+    console.log(newFriend);
+    setFriendName("");
+    setNewImg("https://i.pravatar.cc/48?u=49");
+  }
 
   function handleShowFriends() {
     setShowFriends((prevState) => !prevState);
@@ -30,7 +60,16 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList friends={initialFriends} />
-        {showFriends && <FormAddFriend />}
+        {showFriends && (
+          <FormAddFriend
+            newFriend={friendName}
+            newImg={newImg}
+            handleAddFriend={handleShowFriends}
+            handleSubmit={handleSubmit}
+            handleNameFriend={setFriendName}
+            handleImgFriend={setNewImg}
+          />
+        )}
         <Button onClick={handleShowFriends}>
           {showFriends === false ? "Add Friend" : "Close"}
         </Button>
@@ -82,52 +121,27 @@ function Button({ children, onClick }) {
   );
 }
 
-function FormAddFriend() {
-  const [friendName, setFriendName] = useState("");
-  const [newImg, setNewImg] = useState("https://i.pravatar.cc/48?u=49");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!friendName) {
-      alert("Please enter a name");
-      return;
-    }
-    if (!newImg) {
-      alert("Please enter an image URL");
-      return;
-    }
-    if (!newImg.startsWith("https://")) {
-      alert("Please enter a valid image URL");
-      return;
-    }
-
-    const newFriend = {
-      id: crypto.randomUUID(),
-      name: friendName,
-      image: `${newImg}`,
-      balance: 0,
-    };
-
-    initialFriends.push(newFriend);
-    console.log(newFriend);
-    setFriendName("");
-    setNewImg("https://i.pravatar.cc/48?u=49");
-  }
-
+function FormAddFriend({
+  newFriend,
+  newImg,
+  handleSubmit,
+  handleNameFriend,
+  handleImgFriend,
+}) {
   return (
     <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>Friend name</label>
       <input
         type="text"
-        value={friendName}
-        onChange={(e) => setFriendName(e.target.value)}
+        value={newFriend}
+        onChange={(e) => handleNameFriend(e.target.value)}
       />
 
       <label>🖼️ Image URL</label>
       <input
         type="text"
         value={newImg}
-        onChange={(e) => setNewImg(e.target.value)}
+        onChange={(e) => handleImgFriend(e.target.value)}
       />
 
       <Button>Add</Button>
